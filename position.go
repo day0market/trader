76f3263c9d26f2c609fcd5a927ecb9ccbe3040a8
Alexey Hurko <alex1hurko@gmail.com>
2020-02-15 08:43:33 +0300

    position interface updated

--- position.go
+++ position.go
@@ -1,6 +1,20 @@
 package trader
 
+import "time"
+
+type PositionSide string
+
+const (
+	LongPosition    PositionSide = "Long"
+	ShortPosition   PositionSide = "Short"
+	NotOpenPosition PositionSide = "NotOpen"
+)
+
 type IPosition interface {
+	Instrument() IInstrument
+	OpenTime() time.Time
+	CloseTime() time.Time
+	Side() PositionSide
 	GetOpenSize() int64
 	GetClosedPnL() float64
 	GetOpenPnL() float64
